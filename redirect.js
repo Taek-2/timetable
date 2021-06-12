f5ad551637b0de9e124e01c8e52181aeb7f06222
Oh Euntaek <tagizzang@gmail.com>
2021-06-12 19:37:52 +0900

    2학년 신 링크 업데이트

--- redirect.js
+++ redirect.js
@@ -5,115 +5,127 @@
 by 동탄중앙고 10613 오은택*/
 
 
-function ca() //창체: 온라인 클래스만 시행하는 과목
-{swal("온라인 클래스로 이동할까요?","","success",{
-    buttons: {
-      cancel: "취소",
-      OK: true,
-    }
-  })
-  .then((value) => {
-    switch (value) {
-   
-      case "OK":
-        location.href = "https://hoc41.ebssw.kr/2020dtja1/";
-        break;   
-      default:
-        
-    }
-  });
+
+
+function math() //수학1 : Zoom, 온라인 클래스 병행하는 과목
+    {swal("Zoom에서 출석후 온클 강의를 들어주세요.","","info",{
+        buttons: {
+          cancel: "취소",
+          oncl: { text: "온라인 클래스", value: "oncl",},
+          Zoom: true,
+        }
+      })
+      .then((value) => {
+        switch (value) {
+       
+          case "Zoom":
+            location.href = "https://zoom.us/j/5227990930?pwd=a3Z0RURLNkVXclVwNDBlUTNXMjBqUT09";
+            break;
+       
+          case "oncl":
+            location.href = "https://kyg1.ebsoc.co.kr/class/math78910";
+            break;
+       
+          default:
+            
+        }
+      });
 }
 
-function social() //통합사회: Zoom만 시행하는 과목
+function music() //음악연주: Zoom만 시행하는 과목
 {swal("Zoom으로 이동할까요?","","success",{
-    buttons: {
-      cancel: "취소",
-      OK: true,
-    }
-  })
-  .then((value) => {
-    switch (value) {
-   
-      case "OK":
-        location.href = "https://zoom.us/j/6109037003?pwd=YXVybnZsb1FQL2FBdUF6M0NLNitVUT09";
-        break;   
-      default:
-        
-    }
-  });
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://zoom.us/j/8372687784?pwd=QjRhMkd4aU14azRaQk5CaGtpOFJGZz09";
+      break;   
+    default:
+      
+  }
+});
 }
 
-function history() //한국사: Zoom만 시행하는 과목
+function ABC() //탐구 A/B/C: 과목마다 다름
+    {swal("무슨 과목인가요?","","info",{
+        buttons: {
+          cancel: "취소",
+          physics: { text: "물리학 온클",},
+          chemistry: { text: "화학 줌",},
+          earth: { text: "지구과학 줌",},
+          biology: { text: "생명과학 줌",},
+        }
+      })
+      .then((value) => {
+        switch (value) {
+       
+          case "physics":
+            location.href = "https://kyg1.ebsoc.co.kr/class/dtjaphy1";
+            break;
+
+          case "chemistry":
+            location.href = "https://us02web.zoom.us/j/81255674885?pwd=NzYvb1B3YzB5eVNUR0wybEk0cmRJdz09";
+            break;
+
+          case "earth":
+            location.href = "https://zoom.us/j/3756389007?pwd=UHRpWmVpRDl2M2JOdXdGcWp2OEtTQT09";
+            break;
+
+          case "biology":
+            location.href = "https://zoom.us/j/2075015772?pwd=V1hoY1dUQUJBaGNrL1p1eFk5enRCZz09";
+            break;
+       
+          default:
+            
+        }
+      });
+}
+
+function stactics() //확률과 통계: Zoom만 시행하는 과목
 {swal("Zoom으로 이동할까요?","","success",{
-    buttons: {
-      cancel: "취소",
-      OK: true,
-    }
-  })
-  .then((value) => {
-    switch (value) {
-   
-      case "OK":
-        location.href = "https://zoom.us/j/7122146131?pwd=dy9pakJSUDEvQVNpaGVxOGZGT1F6QT09";
-        break;   
-      default:
-        
-    }
-  });
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://zoom.us/j/9245017232?pwd=N2NrZjlDNXZVd0gveDM3UDFSbU0xdz09";
+      break;   
+    default:
+      
+  }
+});
 }
 
-function math() //수학: 구글 클래스룸, 구글 밋 병행하는 과목
-{swal("어떤 형태의 수업인가요?","","info",{
-    buttons: {
-      cancel: "취소",
-      class: { text: "구글 클래스룸", value: "gocl",},
-      meet: { text: "구글 밋", value: "meet",},
-    }
-  })
-  .then((value) => {
-    switch (value) {
-   
-      case "gocl":
-        location.href = "https://classroom.google.com/c/MTQ5MzQxODQwNDAw";
-        break;
-   
-      case "meet":
-        location.href = "https://meet.google.com/lookup/cqrmert6xg";
-        break;
-   
-      default:
-        
-    }
-  });
+function korean() //문학: Zoom만 시행하는 과목
+{swal("Zoom으로 이동할까요?","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://zoom.us/j/96945493443?pwd=MStKVGk0UzBkWnUzLy9ydkJSY3Fidz09";
+      break;   
+    default:
+      
+  }
+});
 }
 
-function music() //음악: 구글 클래스룸, 구글 밋 병행하는 과목
-{swal("어떤 형태의 수업인가요?","","info",{
-    buttons: {
-      cancel: "취소",
-      class: { text: "구글 클래스룸", value: "gocl",},
-      meet: { text: "구글 밋", value: "meet",},
-    }
-  })
-  .then((value) => {
-    switch (value) {
-   
-      case "gocl":
-        location.href = "https://classroom.google.com/c/MTMxNDk2MTM0MzY0";
-        break;
-   
-      case "meet":
-        location.href = "https://meet.google.com/lookup/dyxd6sphgo";
-        break;
-   
-      default:
-        
-    }
-  });
-}
-
-function science() //통합과학: Zoom, 온라인 클래스 병행하는 과목
-    {swal("어떤 형태의 수업인가요?","","info",{
+function japanese() //일본어1: Zoom, 온라인 클래스 병행하는 과목
+    {swal("Zoom에서 출석후 온클 강의를 들어주세요.","","info",{
         buttons: {
           cancel: "취소",
           oncl: { text: "온라인 클래스", value: "oncl",},
@@ -124,11 +136,11 @@ function science() //통합과학: Zoom, 온라인 클래스 병행하는 과목
         switch (value) {
        
           case "Zoom":
-            location.href = "https://zoom.us/j/3756389007?pwd=M29pZzFRVEtHTkdZSUpVQzlXaHNVQT09";
+            location.href = "https://zoom.us/j/8198690704?pwd=ekdVb2ZwWHdFRjRQSTZkVktEbStaQT09";
             break;
        
           case "oncl":
-            location.href = "https://hoc41.ebssw.kr/atmoslsh/";
+            location.href = "https://kyg1.ebsoc.co.kr/class/195/";
             break;
        
           default:
@@ -137,152 +149,78 @@ function science() //통합과학: Zoom, 온라인 클래스 병행하는 과목
       });
 }
 
-function tamsil() //과학탐구실험: Zoom, 온라인 클래스 병행하는 과목
-    {swal("어떤 형태의 수업인가요?","","info",{
-        buttons: {
-          cancel: "취소",
-          oncl: { text: "온라인 클래스", value: "oncl",},
-          Zoom: true,
-        }
-      })
-      .then((value) => {
-        switch (value) {
-       
-          case "Zoom":
-            location.href = "https://us02web.zoom.us/j/83930270074?pwd=c3VzMmM4NEU2R1FVNitJMVcyalI2QT09";
-            break;
-       
-          case "oncl":
-            location.href = "https://hoc41.ebssw.kr/earthssss20/";
-            break;
-       
-          default:
-            
-        }
-      });
+function english() //영어: Google Meet만 시행하는 과목
+{swal("구글 밋으로 이동할까요?","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://meet.google.com/lookup/h6nngwymq2";
+      break;   
+    default:
+      
+  }
+});
 }
 
-function korean() //국어: Zoom, 온라인 클래스 병행하는 과목
-{swal("어떤 형태의 수업인가요?","","info",{
-        buttons: {
-          cancel: "취소",
-          oncl: { text: "온라인 클래스", value: "oncl",},
-          Zoom: true,
-        }
-      })
-      .then((value) => {
-        switch (value) {
-       
-          case "Zoom":
-            location.href = "https://zoom.us/j/9641411259?pwd=cjMxYUFJYXJPQ0FZTGZ4ZEVRcjVmUT09";
-            break;
-       
-          case "oncl":
-            location.href = "https://hoc41.ebssw.kr/dtkorean/";
-            break;
-       
-          default:
-            
-        }
-      });
+function jinro() //진로: Zoom만 시행하는 과목
+{swal("Zoom으로 이동할까요?","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://zoom.us/j/8372687784?pwd=dWFkc2xTWm43Z1lpZXc0dnJ5aXEzUT09";
+      break;   
+    default:
+      
+  }
+});
 }
 
-function techhome() //기술가정: Zoom, 온라인 클래스 병행하는 과목
-    {swal("어떤 형태의 수업인가요?","","info",{
-        buttons: {
-          cancel: "취소",
-          oncl: { text: "온라인 클래스", value: "oncl",},
-          Zoom: true,
-        }
-      })
-      .then((value) => {
-        switch (value) {
-       
-          case "Zoom":
-            location.href = "https://zoom.us/j/99400167593?pwd=d0Rna1c5czlXV1NPQmlZSW1qNFBzZz09";
-            break;
-       
-          case "oncl":
-            location.href = "https://hoc41.ebssw.kr/teh21/";
-            break;
-       
-          default:
-            
-        }
-      });
+function ca() //창체: Zoom만 시행하는 과목
+{swal("Zoom으로 이동할까요?","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://zoom.us/j/8372687784?pwd=dWFkc2xTWm43Z1lpZXc0dnJ5aXEzUT09";
+      break;   
+    default:
+      
+  }
+});
 }
 
-function english() //영어: Zoom, 온라인 클래스 병행하는 과목
-    {swal("어떤 형태의 수업인가요?","","info",{
-        buttons: {
-          cancel: "취소",
-          oncl: { text: "온라인 클래스", value: "oncl",},
-          Zoom: true,
-        }
-      })
-      .then((value) => {
-        switch (value) {
-       
-          case "Zoom":
-            location.href = "https://zoom.us/j/5211656956?pwd=ZFJ5SHJYeTJMZ3JkbkxnUDcrUlJZUT09";
-            break;
-       
-          case "oncl":
-            location.href = "https://hoc41.ebssw.kr/englishclass1stgrade/";
-            break;
-       
-          default:
-            
-        }
-      });
-}
-
-function jinro() //진로: Zoom, 온라인 클래스 병행하는 과목
-    {swal("어떤 형태의 수업인가요?","","info",{
-        buttons: {
-          cancel: "취소",
-          oncl: { text: "온라인 클래스", value: "oncl",},
-          Zoom: true,
-        }
-      })
-      .then((value) => {
-        switch (value) {
-       
-          case "Zoom":
-            location.href = "https://us02web.zoom.us/j/5013839985?pwd=SVVZSGR1SFRjcmhwdlI4Mmk0RGk3Zz09";
-            break;
-       
-          case "oncl":
-            location.href = "https://hoc41.ebssw.kr/jinroinfo/";
-            break;
-       
-          default:
-            
-        }
-      });
-}
-
-function pe() //체육: Zoom, 온라인 클래스 병행하는 과목
-    {swal("어떤 형태의 수업인가요?","","info",{
-        buttons: {
-          cancel: "취소",
-          oncl: { text: "온라인 클래스", value: "oncl",},
-          Zoom: true,
-        }
-      })
-      .then((value) => {
-        switch (value) {
-       
-          case "Zoom":
-            location.href = "https://us04web.zoom.us/j/8547166710?pwd=bEVuR09DTGllZU56ZnowY2xCbU1aQT09";
-            break;
-       
-          case "oncl":
-            location.href = "https://hoc41.ebssw.kr/dtmhspe1/";
-            break;
-       
-          default:
-            
-        }
-      });
+function pe() //운동과 건강: Zoom만 시행하는 과목
+{swal("Zoom으로 이동할까요?","","success",{
+  buttons: {
+    cancel: "취소",
+    OK: true,
+  }
+})
+.then((value) => {
+  switch (value) {
+ 
+    case "OK":
+      location.href = "https://us04web.zoom.us/j/8547166710?pwd=akZ3ZlZ3TE83T2JoMmZ4eWxnM096dz09";
+      break;   
+    default:
+      
+  }
+});
 }
